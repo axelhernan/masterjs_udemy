0eab3d5f162ebe179017d3123398419e7c5ca520
axelhernan <axeel.hernan@gmail.com>
2020-03-23 21:23:49 -0300

    array course

--- js/curso.js
+++ js/curso.js
@@ -1,15 +1,19 @@
-// funciones texto js
+// for each
 
-//funciones de texto de busqueda 
 'use strict'
 
-var numero = 444;
-var texto1 = "buenvenido al curso de javascript de victor";
-var texto2 = "es un curso bueno";
+var nombre = "Axelin hernan";
+var nombres = ["Axel Hernan", "Roni Ezequiel", "Ramon Antonio", 52, true];
+var lenguajes = new Array("php", "js", "go", "java");
 
-var busqueda = texto1.indexOf("curso"); //busca la primera coincidencia
-    busqueda = texto1.lastIndexOf("curso"); //busca la lastima coincidencia
-    busqueda = texto1.search("curso"); // busca la o las palabras
-    busqueda = texto1.match("curso"); // hace un array con las palabras
+document.write("<h1>lenguajes de programacion del 2018</h1>");
+document.write("<ul>");
 
-console.log(busqueda);
+lenguajes.forEach((elemento, indice, arr)=>{
+    console.log(arr);
+    document.write("<li>" + indice + " " + elemento + "</li>");
+});
+
+
+
+document.write("</ul>")
