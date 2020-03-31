5065beea0fb47527cbec28d005593fdd4994bd6c
axelhernan <axeel.hernan@gmail.com>
2020-03-31 11:33:36 -0300

    aprendiendo doms

--- js/curso.js
+++ js/curso.js
@@ -1,19 +1,32 @@
-// for each
-
+// dom document object mode
 'use strict'
+function cambiacolor(color){
+    caja.style.background = color;
+}
 
-var nombre = "Axelin hernan";
-var nombres = ["Axel Hernan", "Roni Ezequiel", "Ramon Antonio", 52, true];
-var lenguajes = new Array("php", "js", "go", "java");
+// var caja = document.getElementById("micaja");
+var caja = document.querySelector("#micaja");
 
-document.write("<h1>lenguajes de programacion del 2018</h1>");
-document.write("<ul>");
-
-lenguajes.forEach((elemento, indice, arr)=>{
-    console.log(arr);
-    document.write("<li>" + indice + " " + elemento + "</li>");
-});
-
+caja.style.background = "red";
+caja.style.padding = "20px";
+caja.style.color = "white";
+caja.className = "hola"
 
 
-document.write("</ul>")
+console.log(caja);
+
+// meter elementos dentro de una etiqueta 
+var todoslosdivs = document.getElementsByTagName('div');
+var contenidotexto = todoslosdivs[2]; //mostrar el contenido de una etiqueta exacta
+contenidotexto.innerHTML = "boca la concha de tu madre"; //modificar el contenido de ese div exacto
+
+var valor;
+
+for(valor in todoslosdivs){
+    if(typeof todoslosdivs[valor].textContent == 'string'){
+        var parrafo =  document.createElement ("p");
+        var texto = document.createTextNode(todoslosdivs[valor].textContent);
+        parrafo.appendChild(texto); // append añade antes
+        document.querySelector("#miseccion").prepend(parrafo); // prepend añade despues de la etiqueta
+    }
+}
